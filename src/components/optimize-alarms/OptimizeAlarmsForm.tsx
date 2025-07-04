773d70c9359e20867b180f46141e20ee8acc0824
Irineu Marcos <spetoki@gmail.com>
2025-07-04 18:20:26 +0000

    teste

--- src/components/optimize-alarms/OptimizeAlarmsForm.tsx
+++ src/components/optimize-alarms/OptimizeAlarmsForm.tsx
@@ -1,7 +1,7 @@
 
 "use client";
 
-import { useFormState, useFormStatus } from 'react-dom';
+import { useFormState, useFormStatus } from 'react';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Textarea } from '@/components/ui/textarea';
