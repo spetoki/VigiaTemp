0236b6944eff186ec5d6881c54ca89685102a79e
Irineu Marcos <spetoki@gmail.com>
2025-07-24 16:18:51 +0000

    este erro

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-      },
-    ],
+  serverActions: {
+    bodySizeLimit: '5mb',
   },
 };
 
