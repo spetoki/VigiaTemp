fb876c1b0efc6f71a4aa21cd9e1c4f35f41252e5
Irineu Marcos <spetoki@gmail.com>
2025-08-29 15:04:28 +0000

    o que tem de errado

--- src/components/web-flasher/WebFlasher.tsx
+++ src/components/web-flasher/WebFlasher.tsx
@@ -2,9 +2,10 @@
 "use client";
 
 import React from 'react';
-import { Loader2 } from 'lucide-react';
 import { useSettings } from '@/context/SettingsContext';
 import dynamic from 'next/dynamic';
+import { Button } from '../ui/button';
+import { Loader2, Usb } from 'lucide-react';
 
 // O `esp-web-tools` é uma biblioteca que interage diretamente com as APIs do navegador (Web Serial).
 // Para evitar problemas durante a renderização no servidor do Next.js (SSR), onde `window` e `document` não existem,
@@ -21,13 +22,10 @@ const Flasher = dynamic(
       const { t } = useSettings();
       return (
         <div className="flex flex-col items-center justify-center text-center p-6">
-            <button
-              disabled={true}
-              className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-11 rounded-md px-8"
-            >
+            <Button size="lg" disabled={true}>
               <Loader2 className="mr-2 h-4 w-4 animate-spin" />
               {t('webFlasher.connectButton.loading', 'Aguarde...')}
-            </button>
+            </Button>
         </div>
       );
     },
@@ -36,7 +34,6 @@ const Flasher = dynamic(
 
 
 export default function WebFlasher() {
-
   return (
     <div className="w-full flex flex-col items-center justify-center text-center">
         <Flasher />
