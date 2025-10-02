e8787ea1f8922c3d68af84241c9880175fa60d63
Your Name <you@example.com>
2025-10-02 15:52:12 +0000

    atualize o arquivo env com esses valores / Import the functions you need

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,17 +4,17 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBxlONjf4kw4Ix75rGFJVvz_WXSvmorFhw",
-  authDomain: "vigiatemp.firebaseapp.com",
-  databaseURL: "https://vigiatemp-default-rtdb.firebaseio.com",
-  projectId: "vigiatemp",
-  storageBucket: "vigiatemp.firebasestorage.app",
-  messagingSenderId: "75900061601",
-  appId: "1:75900061601:web:ac19591587d74fccdf2de0"
+  apiKey: "AIzaSyCBlH6j-IpV7w3JAwngeWAWZqi5iyftuNY",
+  authDomain: "vigiatemp-91072.firebaseapp.com",
+  projectId: "vigiatemp-91072",
+  storageBucket: "vigiatemp-91072.appspot.com",
+  messagingSenderId: "141850921697",
+  appId: "1:141850921697:web:50280fbc83ee3e5c2305a0",
+  measurementId: "G-F46DRD1DX4"
 };
 
-// Inicializa o Firebase
-// Esta lógica evita que o app seja inicializado várias vezes.
+// Initialize Firebase
+// This logic prevents the app from being initialized multiple times.
 let app;
 if (!getApps().length) {
   app = initializeApp(firebaseConfig);
@@ -24,10 +24,10 @@ if (!getApps().length) {
 
 const db = getFirestore(app);
 
-// Função para obter a instância do Firestore.
+// Function to get the Firestore instance.
 function getDb() {
   if (!db) {
-    throw new Error("O Firestore não foi inicializado. Verifique sua configuração do Firebase.");
+    throw new Error("Firestore has not been initialized. Check your Firebase configuration.");
   }
   return db;
 }
