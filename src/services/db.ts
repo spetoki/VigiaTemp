a9bba74db47e8d81d5e9dcda03eaf273c2314acb
Irineu Marcos <spetoki@gmail.com>
2025-10-04 20:28:00 +0000

    meu id esta como vigiatemp-2af24

--- src/services/db.ts
+++ src/services/db.ts
@@ -4,9 +4,9 @@ import { getFirestore, Firestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyCBlH6j-IpV7w3JAwngeWAWZqi5iyftuNY",
-  authDomain: "vigiatemp-91072.firebaseapp.com",
-  projectId: "vigiatemp-91072",
-  storageBucket: "vigiatemp-91072.appspot.com",
+  authDomain: "vigiatemp-2af24.firebaseapp.com",
+  projectId: "vigiatemp-2af24",
+  storageBucket: "vigiatemp-2af24.appspot.com",
   messagingSenderId: "141850921697",
   appId: "1:141850921697:web:50280fbc83ee3e5c2305a0",
   measurementId: "G-F46DRD1DX4"
